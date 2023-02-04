36788ffee2840acc1ebdff98f3925d9e3850a427
Anton1919 <bf3kenzo@mail.ru>
2023-02-04 14:57:08 +0300

    add tests

--- src/state/task-reducer.ts
+++ src/state/task-reducer.ts
@@ -1,15 +1,23 @@
 import {TasksStateType} from "../App";
 import {v1} from "uuid";
+import {AddTodolistActionType, RemoveTodolistActionType} from "./todolists-reducer";
 
 type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
 type AddTaskActionType = ReturnType<typeof addTaskAC>
 type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
+type ChangeTaskTitleActionType = ReturnType<typeof changeTaskTitleAC>
 
-type ActionType = RemoveTaskActionType | AddTaskActionType | ChangeTaskStatusActionType
+type ActionType = RemoveTaskActionType
+	| AddTaskActionType
+	| ChangeTaskStatusActionType
+	| ChangeTaskTitleActionType
+	| AddTodolistActionType
+	| RemoveTodolistActionType
 
 const REMOVE_TASK = "REMOVE-TASK"
 const ADD_TASK = "ADD-TASK"
 const CHANGE_TASK_STATUS = "CHANGE-TASK-STATUS"
+const CHANGE_TASK_TITLE = "CHANGE-TASK-TITLE"
 
 export const tasksReducer = (state: TasksStateType, action: ActionType) => {
 	switch (action.type) {
@@ -33,9 +41,27 @@ export const tasksReducer = (state: TasksStateType, action: ActionType) => {
 					.map(t => t.id === action.taskId ? {...t, isDone: action.isDone} : t)
 			}
 
+		case CHANGE_TASK_TITLE:
+			return {
+				...state,
+				[action.todoListId]: state[action.todoListId]
+					.map(t => t.id === action.taskId ? {...t, title: action.title} : t)
+			}
+
+		case 'ADD-TODOLIST':
+			return {
+				...state,
+				[action.todolistId]: []
+			}
+
+		case 'REMOVE-TODOLIST': {
+			const copyState = {...state}
+			delete copyState[action.id]
+			return copyState
+		}
+
 		default:
 			return state
-
 	}
 }
 
@@ -49,4 +75,8 @@ export const addTaskAC = (title: string, todoListId: string) => ({
 
 export const changeTaskStatusAC = (taskId: string, isDone: boolean, todoListId: string) => ({
 	type: CHANGE_TASK_STATUS, taskId, isDone, todoListId
+} as const)
+
+export const changeTaskTitleAC = (taskId: string, title: string, todoListId: string) => ({
+	type: CHANGE_TASK_TITLE, taskId, title, todoListId
 } as const)
