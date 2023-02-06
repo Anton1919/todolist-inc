2d69550768905f5aa7b55e376dc063cbab0d1d61
Anton1919 <bf3kenzo@mail.ru>
2023-02-06 19:00:33 +0300

    add useReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
+import AppWithRedux from "./AppWithReducer";
 
 const container  = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<App />);
+root.render(<AppWithRedux />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
